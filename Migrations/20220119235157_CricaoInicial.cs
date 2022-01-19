0f7a6b4ec4fab2e07f6df44de2805f7e87c343b8
Raul Marciano <raul_marciano@outlook.com>
2022-01-19 20:58:12 -0300

    Criação da primeira Migration com o Cliente

---
+++ Migrations/20220119235157_CricaoInicial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DesafioChallengeSPA.Migrations
+{
+    public partial class CricaoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Sobrenome = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "DATE", nullable: false),
+                    Idade = table.Column<int>(type: "int", nullable: false),
+                    ProfissaoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
